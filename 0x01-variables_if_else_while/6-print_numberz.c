28172286fc5be96588aa6e0de411712cd469fea0
martine-K <kungumartine06@gmail.com>
2023-09-08 04:36:52 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,15 +8,15 @@
  * Return: Always 0 (Succes)
  */
 
-
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+	}
 	putchar('\n');
-
 	return (0);
 }
+
