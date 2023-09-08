4ef103fb5109faa4890a1d5553ac68692909d329
martine-K <kungumartine06@gmail.com>
2023-09-08 03:15:59 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,34 @@
-/*
+/**
  * File: 0-positive_or_negative.c
- * Auth:Martine Kungu
+ * Auth: Martine Kungu
  */
-
-
-
 
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * This program determines if a randomly generated number
+ * is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
+    if (n > 0)
+        printf("%d is positive\n", n);
+    else if (n < 0)
+        printf("%d is negative\n", n);
+    else
+        printf("%d is zero\n", n);
 
-	return (0);
+    return (0);
 }
